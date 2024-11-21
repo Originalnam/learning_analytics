6681742cc7cf8f40e8b0505c85c1fe00a91500db
YVDA <Originalnam@users.noreply.github.com>
2024-11-20 21:55:18 -0800

    EDA done

--- EDA/eda.py
+++ EDA/eda.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
+from pathlib import Path
 
 # load data
-path = '../data/'
+script_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(script_dir, '../data/')
 files = os.listdir(path)
 csv_files = [file for file in files if file.endswith('.csv')]
 table_names = [file.split('.')[0] for file in csv_files]
@@ -13,13 +15,21 @@ for table_name in table_names:
     dfs[f'{table_name}'] = pd.read_csv(f'{path}{table_name}.csv')
 
 # data manipulation
-## add presentation month to courses
+df_courses = dfs['courses']
+df_assessments = dfs['assessments']
+df_vle = dfs['vle']
+df_studentinfo = dfs['studentInfo']
+df_studentregistration = dfs['studentRegistration']
+df_studentassessment = dfs['studentAssessment']
+df_studentvle = dfs['studentVle']
+
+# add presentation month to courses
 df_courses = dfs['courses']
 df_courses[['year', 'start_month']] = df_courses['code_presentation'].str.extract(r'(\d{4})([A-Za-z])')
 df_courses.loc[df_courses['start_month'] == 'J', 'start_month'] = 'October'
-df_courses.loc[df_courses['start_month'] == 'B', 'steart_month'] = 'February'
+df_courses.loc[df_courses['start_month'] == 'B', 'start_month'] = 'February'
 
-## change missing date of Exam assessments to last day of course
+# change missing date of Exam assessments to last day of course
 df_assessments = dfs['assessments']
 df_assessments.loc[
     (df_assessments['date'].isna()) & 
@@ -30,4 +40,43 @@ df_assessments.loc[
         on=['code_module', 'code_presentation']
         )['module_presentation_length']
 
-## vle
+# move changing columns from studentinfo to studenregistration 
+df_studentregistration = df_studentregistration.merge(
+    df_studentinfo[
+        ['id_student',
+         'age_band', 
+         'imd_band', 
+         'studied_credits', 
+         'num_of_prev_attempts', 
+         'final_result', 
+         'code_module', 
+         'code_presentation']
+         ], 
+         on=['id_student', 
+             'code_module', 
+             'code_presentation'
+             ]
+             )
+df_studentinfo = df_studentinfo.drop(
+    columns=[
+        'code_module',
+        'code_presentation',
+        'imd_band', 
+        'age_band', 
+        'num_of_prev_attempts', 
+        'studied_credits', 
+        'final_result'
+        ]
+        ).drop_duplicates()
+
+# write data
+write_path = os.path.join(script_dir, '../data/EDA/')
+Path(write_path).mkdir(parents=True, exist_ok=True)
+
+df_courses.to_csv(f'{write_path}courses.csv')
+df_assessments.to_csv(f'{write_path}assessments.csv')
+df_vle.to_csv(f'{write_path}vle.csv')
+df_studentinfo.to_csv(f'{write_path}studentinfo.csv')
+df_studentregistration.to_csv(f'{write_path}studentregistration.csv')
+df_studentassessment.to_csv(f'{write_path}studentassessment.csv')
+df_studentvle.to_csv(f'{write_path}studentvle.csv')
--- README.md
+++ README.md
@@ -3,15 +3,20 @@ This repository uses the [Open University Learning Analytics Dataset](https://an
 
 ## Directories
 * /data
-Contains the original .csv files from the [Open University Learning Analytics Dataset](https://analyse.kmi.open.ac.uk/open_dataset). These are not uploaded in this repository.
+Contains the original .csv files from the [Open University Learning Analytics Dataset](https://analyse.kmi.open.ac.uk/open_dataset). These are not uploaded in this repository. Manipulated data is saved in subfolders (see below)
 
 * /EDA
 General exploratory data analysis of the database
 
 ## EDA
-
+Exploratory data analysis. Changes to data are saved in EDA subfolder in data folder.
 * eda.ipynb:
+Exploratory data analysis of each table and some deeper dives on into some relationships. Limited data manipulation.
 
-Exploratory data analysis of each table.
+* eda.py:
+Copy op manipulations performed in eda.ipynb
+
+
+
 
 
