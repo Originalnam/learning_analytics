0a08cbb502338675372ad3cf95fc13cc0b25158a
YVDA <Originalnam@users.noreply.github.com>
2024-11-21 17:43:55 -0800

    dashboard: region map grouped
    created two geojson files for maps of different granularity

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ This repository uses the [Open University Learning Analytics Dataset](https://an
 
 ## Directories
 * /data
-Contains the original .csv files from the [Open University Learning Analytics Dataset](https://analyse.kmi.open.ac.uk/open_dataset). These are not uploaded in this repository. Manipulated data is saved in subfolders (see below)
+Contains the original .csv files from the [Open University Learning Analytics Dataset](https://analyse.kmi.open.ac.uk/open_dataset). These are not uploaded in this repository. Manipulated data is saved in subfolders in data. Download the original csv files and run the .py files to get all intermediate data manipulations.
 
 * /EDA
 General exploratory data analysis of the database
+
+* /dashboard
+Creation of dashboards based on the datasets
 
 ## EDA
 Exploratory data analysis. Changes to data are saved in EDA subfolder in data folder.
@@ -15,6 +18,10 @@ Exploratory data analysis of each table and some deeper dives on into some relat
 
 * eda.py:
 Copy op manipulations performed in eda.ipynb
+
+## dasboard
+
+
 
 
 
--- dashboard/student_region_map.py
+++ dashboard/student_region_map.py
@@ -6,7 +6,7 @@ import json
 
 app = Dash(__name__)
 
-data = {
+data_regions = {
     "region": ['North East',
     'North West',
     'Yorkshire and The Humber',
@@ -21,21 +21,83 @@ data = {
     'Outline of Northern Ireland'],
     "value": list(range(1,13)),
     }
-studentdata = pd.DataFrame(data)
+studentdata_regions = pd.DataFrame(data_regions)
 
-with open('./data/combined_geojson.geojson', 'r') as file:
-    geojson = json.load(file)
+with open('./data/allregions.geojson', 'r') as file:
+    geojson_regions = json.load(file)
 
+data_regions_grouped = {
+    "region": ['East Midlands',
+               'North East',
+               'Outline of Northern Ireland',
+               'Scotland',
+               'Eastern',
+               'Wales',
+               'West Midlands',
+               'North Region',
+               'South Region'
+               ],
+    "value": list(range(5,14)),
+    }
+studentdata_regions_grouped = pd.DataFrame(data_regions)
 
-fig = px.choropleth_map(studentdata, geojson=geojson, locations='region', color='value', featureidkey="properties.NAME",
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           map_style="carto-positron",
-                           zoom=4, center = {"lat": 55.6187, "lon": -1.3698},
-                           opacity=0.5,
-                           labels={'unemp':'unemployment rate'}
-                          )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+with open('./data/allregions_grouped.geojson', 'r') as file:
+    geojson_regions_grouped = json.load(file)
 
+# Layout
+app.layout = html.Div([
+    # Dropdown for selection
+    html.Div([
+        dcc.Dropdown(
+            id='data-selector',
+            options=[
+                {'label': 'Regions', 'value': 'regions'},
+                {'label': 'Groups', 'value': 'groups'}
+            ],
+            value='groups',
+            style={'width': '200px', 'margin': '10px'}
+        )
+    ]),
+    
+    # Graph
+    dcc.Graph(id='choropleth-map')
+])
+
+@callback(
+    Output('choropleth-map', 'figure'),
+    Input('data-selector', 'value')
+)
+def update_map(selected_data):
+    # Select the appropriate dataset based on dropdown value
+    if selected_data == 'regions':
+        df = data_regions
+        geojson = geojson_regions
+    else:
+        df = data_regions_grouped
+        geojson = geojson_regions_grouped
+    
+    title = 'Regions Map'
+
+    # Create the figure
+    fig = px.choropleth_map(df, geojson=geojson, 
+                            locations='region', 
+                            color='value', 
+                            featureidkey="properties.region",
+                            color_continuous_scale="brwnyl",
+                            range_color=(0, 12),
+                            map_style="carto-positron",
+                            zoom=4, center = {"lat": 55.6187, "lon": -1.3698},
+                            opacity=0.5,
+                            labels={'unemp':'unemployment rate'}
+                            )
+    
+    fig.update_layout(
+        margin={"r": 0, "t": 30, "l": 0, "b": 0},
+        title=title
+    )
+    
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
